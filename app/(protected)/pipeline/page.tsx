9356f7d40af52977e5f527cee119e3d781e5e3ca
Matheus Willock <matheuswillock@gmail.com>
2025-09-11 20:20:16 -0300

    feat: add pipeline page with data table component
    - Implemented a new Pipeline page that utilizes a DataTable component to display data from a JSON file.
    - Created DataTable component with drag-and-drop functionality using dnd-kit.
    - Integrated various UI components including Checkbox, Drawer, and Table for enhanced user interaction.
    - Added support for sorting, filtering, and pagination in the DataTable.
    - Established a schema for data validation using zod.
    - Included chart visualization for data insights within the Drawer component.

---
+++ app/(protected)/pipeline/page.tsx
@@ -0,0 +1,11 @@
+import { DataTable } from "@/components/data-table";
+import data from "./data.json"
+
+export default async function Pipeline() {
+
+    return (
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <DataTable data={data} />
+        </div>
+    )
+}
